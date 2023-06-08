bc4e462a7711c3d73cfa83a3b330bc76f91c3795
Paul Murray <paulgelstonmurray@gmail.com>
2023-06-08 16:34:36 -0400

    feat(build): Correct URL

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: "/test/",
+  base: "/bottlenecks/",
   css: {
     postcss: {
       plugins: [
