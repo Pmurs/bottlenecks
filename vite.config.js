4ebc03c841663395d4fe19acbcdace08e0490653
Paul Murray <paulgelstonmurray@gmail.com>
2023-06-08 15:48:54 -0400

    feat(build): Postcss

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import postcssNesting from 'postcss-nesting';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +13,11 @@ export default defineConfig({
     },
   },
   base: "/bottlenecks/",
+  css: {
+    postcss: {
+      plugins: [
+        postcssNesting
+      ],
+    },
+  },
 });
