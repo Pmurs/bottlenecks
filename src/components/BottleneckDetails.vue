368617be1cb854d461b3629dd6ffdfac5eafca6f
Paul Murray <paulgelstonmurray@gmail.com>
2023-11-30 20:29:17 -0500

    feat(Responses): List formatting, add tag info for responses

--- src/components/BottleneckDetails.vue
+++ src/components/BottleneckDetails.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts">
+<script setup>
+import tagLabels from "@/util/tagLabels.json";
+
 const props = defineProps({
   data: {
     title: String,
@@ -15,28 +17,56 @@ const props = defineProps({
     <h2>{{ data.description }}</h2>
     <div>Count: {{ data.items.length }}</div>
     <div class="answers">Answers:</div>
-    <div v-for="(item, index) in data.items" v-bind:key="index" class="answer">
-      <div v-if="data.isBottleneck">
-        <h3>{{ index + 1 }}.</h3>
-        <div>{{ "Bottleneck: " + item.title }}</div>
-        <div v-html="'Why: ' + item.description"></div>
-      </div>
-      <div v-else>
-        <div>{{ index + 1 + ". " + item.title }}</div>
-        <!--        <div v-for="(investment, index2) in item.investment?.split(/(?<!scale),/)" v-bind:key="index2">-->
-        <!--          <div>{{ "Investment: " + investment }}</div>-->
-        <!--        </div>-->
-      </div>
-    </div>
+    <ol class="answers-list">
+      <li v-for="(item, index) in data.items" v-bind:key="index" class="answer">
+        <div v-if="data.isBottleneck">
+          <div>{{ "Bottleneck: " + item.title }}</div>
+          <div v-html="'Why: ' + item.description"></div>
+        </div>
+        <div v-else>
+          <div>{{ index + 1 + ". " + item.title }}</div>
+          <!--        <div v-for="(investment, index2) in item.investment?.split(/(?<!scale),/)" v-bind:key="index2">-->
+          <!--          <div>{{ "Investment: " + investment }}</div>-->
+          <!--        </div>-->
+        </div>
+        <div>Tags:</div>
+        <ul
+          class="tags-list"
+          v-for="(tag, index) in item.tags"
+          v-bind:key="index"
+        >
+          <li>{{ tag + " " + tagLabels[tag].label }}</li>
+        </ul>
+      </li>
+    </ol>
   </div>
 </template>
 
 <style scoped>
 .answers {
-  margin-bottom: 1em;
+  margin-bottom: 2em;
 }
 
-.answer {
-  margin-bottom: 1em;
+.answers-list {
+  padding-left: 0.5em;
+
+  .answer {
+    margin-bottom: 1em;
+  }
+
+  .tags-list {
+    margin: 0;
+    padding-left: 1em;
+    list-style-type: none;
+
+    li {
+      text-indent: -5px;
+
+      &:before {
+        content: "- ";
+        text-indent: -5px;
+      }
+    }
+  }
 }
 </style>
